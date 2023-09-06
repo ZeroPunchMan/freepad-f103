86ede46f23b43790337a5e6b97d7e75f04148227
lyj <knightlyj@163.com>
2023-09-06 10:32:19 +0800

    增加pad_func

---
+++ firmware/app/Application/pad_func.c
@@ -0,0 +1,41 @@
+#include "pad_func.h"
+#include "main.h"
+#include "crc.h"
+#include "string.h"
+#include "flash_layout.h"
+#include "iflash_stm32.h"
+
+typedef struct
+{
+    uint16_t leftX[2], leftY[2];
+    uint16_t rightX[2], rightY[2];
+    uint16_t leftTrigger[2], rightTrigger[2];
+    uint32_t crc;
+} CaliParams_t;
+static CaliParams_t caliParams = {0};
+static void LoadCalibration(void)
+{
+    memcpy((void *)&caliParams, (void *)PAD_PARAM_ADDR, sizeof(caliParams));
+    uint32_t crc = Ethernet_CRC32((const uint8_t *)&caliParams, CL_OFFSET_OF(CaliParams_t, crc));
+    if (crc != caliParams.crc)
+    { // use default params  todo
+    }
+}
+
+static void SaveCalibration(void)
+{
+    caliParams.crc = Ethernet_CRC32((const uint8_t *)&caliParams, CL_OFFSET_OF(CaliParams_t, crc));
+    HAL_FLASH_Unlock();
+    IFlashStm32_ErasePages(PAD_PARAM_ADDR, 1);
+    IFlashStm32_Write(PAD_PARAM_ADDR, (const uint8_t *)&caliParams, sizeof(caliParams));
+    HAL_FLASH_Lock();
+}
+
+void PadFunc_Init(void)
+{
+    LoadCalibration();
+}
+
+void PadFunc_Process(void)
+{
+}
---
+++ firmware/app/Application/pad_func.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "cl_common.h"
+
+void PadFunc_Init(void);
+void PadFunc_Process(void);
+
