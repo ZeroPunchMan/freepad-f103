85f21fff417068f78d51727cb7ed56b19bf23cf0
lyj <knightlyj@163.com>
2023-09-08 18:18:44 +0800

    关USB低功耗

--- firmware/boot/Core/Src/stm32f1xx_it.c
+++ firmware/boot/Core/Src/stm32f1xx_it.c
@@ -26,9 +26,6 @@
 #include "systime.h"
 /* USER CODE END Includes */
 
-/* External functions --------------------------------------------------------*/
-void SystemClock_Config(void);
-
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN TD */
 
@@ -235,26 +232,6 @@ void USART1_IRQHandler(void)
       LL_USART_DisableIT_TXE(USART1);
   }
   /* USER CODE END USART1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USB wake-up interrupt through EXTI line 18.
-  */
-void USBWakeUp_IRQHandler(void)
-{
-  /* USER CODE BEGIN USBWakeUp_IRQn 0 */
-
-  /* USER CODE END USBWakeUp_IRQn 0 */
-  if ((&hpcd_USB_FS)->Init.low_power_enable) {
-    /* Reset SLEEPDEEP bit of Cortex System Control Register */
-    SCB->SCR &= (uint32_t)~((uint32_t)(SCB_SCR_SLEEPDEEP_Msk | SCB_SCR_SLEEPONEXIT_Msk));
-    SystemClock_Config();
-  }
-  /* Clear EXTI pending bit */
-  __HAL_USB_WAKEUP_EXTI_CLEAR_FLAG();
-  /* USER CODE BEGIN USBWakeUp_IRQn 1 */
-
-  /* USER CODE END USBWakeUp_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
